750665065ffcc04448f520532894fa635d342953
Mame Coumba Ka <mck@mames-mbp-2.ads.northwestern.edu>
2022-06-10 15:12:38 -0500

    upd

--- lab10/src/AddComment.js
+++ lab10/src/AddComment.js
@@ -29,7 +29,7 @@ class AddComment extends React.Component {
     
       handleSubmit(event) {
         event.preventDefault(); 
-        const url = '/api/comments';
+        const url = '/https://photo-app-secured.herokuapp.com//comments';
         const postData = {
             post_id: this.props.postId,
             text: this.state.value
@@ -79,7 +79,7 @@ class AddComment extends React.Component {
                 onClick={this.focusTextInput} 
                 aria-label="Submit Comment"
                 onKeyDown={this.handleKeyDown}>
-                
+
                  Post</button>
         </form>
         )
